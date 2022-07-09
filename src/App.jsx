4bc6c5df90206d7b281863364f23d5d8fed328e9
AlamoVinicius <alamo.cma@gmail.com>
2022-07-09 13:32:42 -0300

    feat: configruações inicias de prettier e eslint

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>deu bom no preettier e no eslintrc</h1>
+    </div>
+  );
+}
+
+export default App;
