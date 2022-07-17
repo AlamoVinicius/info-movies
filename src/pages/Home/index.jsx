4d778f58867b3dd613d9add82550169a4ad08de9
AlamoVinicius <alamo.cma@gmail.com>
2022-07-16 23:47:14 -0300

    feat: new functions to render in screen

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,19 +8,26 @@ const Home = () => {
   const [topMovies, setTopMovies] = useState([]);
 
   const getTopRatedMovies = async (url) => {
-    const res = await fetch(url)
-    const data = await res.json()
-
-    setTopMovies(data)
-  }
+    const res = await fetch(url);
+    const { results } = await res.json();
+    // console.log(data.results[0].)
+    setTopMovies(results);
+  };
 
   useEffect(() => {
-      const topRatedUrl = `${moviesUrl}top_rated?${apiKey}` 
-      getTopRatedMovies(topRatedUrl)
-    }, [])
-  
+    const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`;
+    getTopRatedMovies(topRatedUrl);
+  }, []);
 
-  return <div>{JSON.stringify(topMovies)}</div>;
+  return (
+    <div className="container">
+      <h2 className="title">Melhores Filmes</h2>
+      <div className="moviesContainer">
+        {topMovies === 0 && <p>Carregando....</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
